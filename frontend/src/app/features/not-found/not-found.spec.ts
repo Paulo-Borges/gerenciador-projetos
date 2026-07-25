b25d42c7fce41c0cbaa10c26692cb6057011a048
feelipefreitas <feelipe.freitas97@gmail.com>
2026-07-25 13:24:31 -0300

    Lendo o DOM e Buscando Elementos

--- frontend/src/app/features/not-found/not-found.spec.ts
+++ frontend/src/app/features/not-found/not-found.spec.ts
@@ -21,4 +21,18 @@ describe('NotFound Component', () => {
   it('deve criar um componente com sucesso', () => {
     expect(component).toBeTruthy();
   });
+
+  it('deve exibir o título de Página não encontrada', () => {
+    const html: HTMLElement = fixture.nativeElement;
+    const tituloH1 = html.querySelector('h1');
+
+    expect(tituloH1?.textContent).toBe('Página não encontrada');
+  });
+
+  it('deve ter um link de redirecionamento para a páginal inicial (/)', () => {
+    const html: HTMLElement = fixture.nativeElement;
+    const linkA = html.querySelector('a');
+
+    expect(linkA?.getAttribute('href')).toBe('/');
+  });
 });
